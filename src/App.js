424653b7421dc549c39aa8d1f17c4032b9cb1428
rifqifazzam <rifqifauzan709@gmail.com>
2023-02-16 12:18:25 +0700

    [ADD] Footer App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './component/Footer/Footer';
 import Page from './component/Homepage/Page'
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Page />
+      <Footer />
     </div>
   );
 }
