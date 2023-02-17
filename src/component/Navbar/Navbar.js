13f47094856faaf2814d94aa131916ea1064e8a9
ayyasmumtaz <95177018+ayyasmumtaz@users.noreply.github.com>
2023-02-17 21:25:31 +0700

    Update

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -10,13 +10,13 @@ const Navbar = () => {
                     <img src={himalkom} alt='himalkom'></img>
                 </div>
                 <div className="navbar-list">
-                    <li>Home</li>
-                    <li>Profil</li>
-                    <li>Komnews</li>
-                    <li>Galeri</li>
-                    <li>Program Kerja</li>
-                    <li>Komunitas</li>
-                    <li>Riset</li>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#Profil">Profil</a></li>
+                    <li><a href="#Komnews">Komnews</a></li>
+                    <li><a href="#Galeri">Galeri</a></li>
+                    <li><a href="#Program Kerja">Program Kerja</a></li>
+                    <li><a href="#Komunitas">Komunitas</a></li>
+                    <li><a href="#Riset">Riset</a></li>
                 </div>
             </div>
         </div>
