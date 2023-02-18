be41673a9bf285ac7f4faf10792bd7bc6de0da66
rifqifazzam <rifqifauzan709@gmail.com>
2023-02-18 15:23:31 +0700

    [UPDATE] Megaproker

--- src/features/Megaproker/Megaproker.js
+++ src/features/Megaproker/Megaproker.js
@@ -11,20 +11,28 @@ const Megaproker = () => {
             </div>
             <div className="main">
                 <div className="main-ittoday">
-                    <h1>IT TODAY</h1>
-                    <img src={ittoday} alt="ittoday" />
-                    <p>IT Today merupakan serangkaian acara nasional yang bertemakan teknologi informasi dalam dunia Agriculture. Kegiatan ini terdiri atas HackToday (Capture the Flag), UXToday (UI/UX Design), Poster Competition, Workshop. Festival, dan National Seminar mengenai teknologi informasi terkini.
-                    </p>
-                    {/* ini ntar dijadiin button yah */}
-                    <button  className='button'>Selengkapnya</button>
+                    <div className='proker--logo--container'>
+                        <img src={ittoday} alt="ittoday" className='proker--logo'/>
+                    </div>
+                    <div className='proker--container'>
+                        <h1>IT TODAY</h1>
+                        <p className='proker--desc'><span className='bold'>IT Today</span> merupakan serangkaian acara nasional yang bertemakan teknologi informasi dalam dunia Agriculture. Kegiatan ini terdiri atas HackToday (Capture the Flag), UXToday (UI/UX Design), Poster Competition, Workshop. Festival, dan National Seminar mengenai teknologi informasi terkini.
+                        </p>
+                        {/* ini ntar dijadiin button yah */}
+                        <button className='button'>Selengkapnya</button>
+                    </div>
                 </div>
-                <div className="main-Riset">
-                    <h1>RISET ILKOMERZ</h1>
-                    <img src={riset} alt="riset ilkomerz" />
-                    <p> Riset Ilkomerz merupakan kegiatan riset yang ditujukan untuk menjaring minat, pendapat, dan aspirasi ilkomerz untuk membangun Himalkom berdasarkan kebutuhan ilkomerz.
-                    </p>
-                    {/* ini ntar dijadiin button yah */}
-                    <button className='button'>Selengkapnya</button>
+                <div className="main-riset">
+                    <div className='proker--logo--container'>
+                        <img src={riset} alt="riset ilkomerz" className='proker--logo' />
+                    </div>
+                    <div className='proker--container'>
+                        <h1 className='proker--judul'>RISET ILKOMERZ</h1>
+                        <p className='proker--desc'> <span className='bold'>Riset Ilkomerz</span> merupakan kegiatan riset yang ditujukan untuk menjaring minat, pendapat, dan aspirasi ilkomerz untuk membangun Himalkom berdasarkan kebutuhan ilkomerz.
+                        </p>
+                        {/* ini ntar dijadiin button yah */}
+                        <button className='button'>Selengkapnya</button>
+                    </div>
                 </div>
             </div>
         </div>
