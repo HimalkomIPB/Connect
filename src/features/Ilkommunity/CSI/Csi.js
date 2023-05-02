186359a5dd17fce74aa91d447f0583db10e14db9
khalidziarabbani <khalidziarabbani@apps.ipb.ac.id>
2023-05-02 17:05:34 +0700

    add file

---
+++ src/features/Ilkommunity/CSI/Csi.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Csi.css'
+import Navbar from '../../component/Navbar/Navbar'
+import Footer from '../../component/Footer/Footer'
+
+const Csi = () => {
+    return (
+        <div>
+            <Navbar />
+
+        <li>sdsakdns</li>
+
+
+
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Csi
