3213166a0a7dfeba91772adb7b8a292dc2db2e14
ayyasmumtaz <yazmumtaz@gmail.com>
2023-07-09 18:21:24 +0700

    Update IGallery

--- src/features/IGallery/IGallery.js
+++ src/features/IGallery/IGallery.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from "../../component/Navbar/Navbar";
 import Footer from "../../component/Footer/Footer";
+import "./IGallery.css"
 const IGallery = () => {
   return (
     <div>
         <Navbar />
+        <section>
+                <div id="IG-header-container">
+                    <div className="IG-header-wrapper">
+                        <div className="IG-header">
+                            <h2>I-GALLERY</h2>
+                            <p>I-Gallery adalah platform yang berisi kumpulan projek-projek yang telah dibuat oleh mahasiswa ilmu komputer. I-Gallery menampilkan projek dari setiap mata kuliah berprojek. Tujuan dari I-Gallery adalah untuk menunjukan projek yang telah dibuat oleh mahasiswa ilmu komputer.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         <Footer />
     </div>
   )
---
+++ src/features/IGallery/IGallery_desc.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IGallery_desc = () => {
+  return (
+    <div>IGallery_desc</div>
+  )
+}
+
+export default IGallery_desc
