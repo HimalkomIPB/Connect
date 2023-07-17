d5f56725b1a3b1aa5df6a518f03664f11825a329
rifqifazzam <rifqifauzan709@gmail.com>
2023-07-17 17:08:50 +0700

    Riset page responsive + fix footer

--- Frontend/src/features/Riset/Riset.js
+++ Frontend/src/features/Riset/Riset.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './Riset.css';
 import Navbar from "../../component/Navbar/Navbar";
 import Footer from "../../component/Footer/Footer";
-import HeaderIcon from '../../Assets/Riset/Hero-Icon.svg';
 import icon1 from '../../Assets/Riset/Riset-Icon1.svg';
 import icon2 from '../../Assets/Riset/Riset-Icon2.svg';
 import icon3 from '../../Assets/Riset/Riset-Icon3.svg';
@@ -25,9 +24,6 @@ const Riset = () => {
                             <h2>Riset Ilkomerz</h2>
                             <p> <b>Riset Ilkomerz </b>merupakan kegiatan riset yang ditujukan untuk menjaring minat, pendapat, dan aspirasi ilkomerz untuk membangun Himalkom berdasarkan kebutuhan ilkomerz.</p>
                         </div>
-                        <div className="Riset-Icon">
-                            <img src={HeaderIcon} alt="Riset icon" />
-                        </div>
                     </div>
                 </div>
             </section>
@@ -36,9 +32,6 @@ const Riset = () => {
             <section>
                 <div className="Riset-Main-Container">
                     <div className="Riset-Main-Wrapper">
-                        <div id="Riset-Main-Header">
-                            <h2>RISET ILKOMERZ</h2>
-                        </div>
                         <div id="Riset-Main-Column">
                             <div id="Riset">
                                 <img src={icon1} alt="Riset Icon 1"/>
@@ -99,19 +92,7 @@ const Riset = () => {
                 </div>
             </section>
 
-            <section>
-                <div className="Riset-Contact-Container">
-                    <div className="Riset-Contact-Wrapper">
-                        <div id="Riset-Contact-Header">
-                            <h2>Narahubung</h2>
-                        </div>
-                        <div id="Riset-Contact-Person">
-                            <p>08xxxxxxxxx (WhatsApp) Ariq Lubis (Kadiv Ristek)</p>
-                            <p>08xxxxxxxxx (WhatsApp) Ariq Lubis (Kadiv Ristek)</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            
             <Footer />
         </div>
     )
