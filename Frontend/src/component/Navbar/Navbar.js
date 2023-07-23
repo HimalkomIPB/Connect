f3dc315da2d29ab128f5c9ebb93665d248a745ab
GirasArya <girasarya130@gmail.com>
2023-07-23 23:27:52 +0700

    Fixed Mobile layout for ilkommunity

--- Frontend/src/component/Navbar/Navbar.js
+++ Frontend/src/component/Navbar/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
     <div>
       <div className={`navbar ${Active ? "darken" : ""}`}>
         <div className="navbar-heading">
-          <img src={himalkom} alt="himalkom" />
+          <a href="/"> <img src={himalkom} alt="himalkom" /></a>
         </div>
         {/* Mobile Nav */}
         <div className="navbar-state" onClick={activeState}>
