c7e0178cd0398f976bd438183146051a1d03897d
rifqifazzam <rifqifauzan709@gmail.com>
2023-07-25 16:44:32 +0700

    Riset page edit+ add riset data placeholder

---
+++ Frontend/src/Data/Riset_data.js
@@ -0,0 +1,57 @@
+export default [
+    
+        {
+            id:1,
+            title:"Ketertarikan Ilkomerz dalam membangun Startup",
+            date:"23/12",
+            img:"Riset-Icon1"
+        },
+        {
+            id:2,
+            title:"Ketertarikan Ilkomerz dalam membangun Startup",
+            date:"23/12",
+            img:"Riset-Icon2"
+        },
+        {
+            id:3,
+            title:"Ketertarikan Ilkomerz dalam membangun Startup",
+            date:"23/12",
+            img:"Riset-Icon3"
+        },
+        {
+            id:4,
+            title:"Ketertarikan Ilkomerz dalam membangun Startup",
+            date:"23/12",
+            img:"Riset-Icon4"
+        },
+
+        {
+            id:5,
+            title:"Ketertarikan Ilkomerz dalam membangun Startup",
+            date:"23/12",
+            img:"Riset-Icon5"
+        },
+        {
+            id:6,
+            title:"Ketertarikan Ilkomerz dalam membangun Startup",
+            date:"23/12",
+            img:"Riset-Icon6"
+        },
+        {
+            id:7,
+            title:"Ketertarikan Ilkomerz dalam membangun Startup",
+            date:"23/12",
+            img:"Riset-Icon7"
+        },
+        {
+            id:7,
+            title:"Ketertarikan Ilkomerz dalam membangun Startup",
+            date:"23/12",
+            img:"Riset-Icon8"
+        },
+
+
+
+    
+    
+]
