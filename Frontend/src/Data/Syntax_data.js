dd5c4ccae5133af30ea90634c4ba3434b4899130
ayyasmumtaz <yazmumtaz@gmail.com>
2023-12-28 17:03:21 +0700

    add Syntax card + Syntax data

---
+++ Frontend/src/Data/Syntax_data.js
@@ -0,0 +1,68 @@
+export default [
+    
+    {
+        id:1,
+        title:"Survei Minat Jenjang Karir Mahasiswa Ilmu Komputer IPB",
+        date:"28/03/23",
+        img:"minat-jenjang-karir",
+        link: "https://drive.google.com/file/d/1QtElmk9x6ZFr4-vH2v3t1M0llb5IwFIP/view?usp=sharing"
+    },
+    {
+        id:2,
+        title:"Survei Media Penyebaran Informasi dan Kebutuhan Mahasiswa IPB dalam Persiapan GEMASTIK 2023",
+        date:"03/04/23",
+        img:"persiapan-gemastik",
+        link:"https://drive.google.com/file/d/1KMFWDRe6rpVLIVcVHYruCm98kEXZ5o7K/view?usp=sharing"
+    },
+    {
+        id:3,
+        title:"Persiapan dan Kebutuhan Ilkomerz dalam Menghadapi Gemastik 2022",
+        date:"02/07/23",
+        img:"persiapan-kebutuhan-gemastik",
+        link:"https://drive.google.com/file/d/1VGgSDcEbKtRDR-TcSViraZDkbYHLCGOZ/view?usp=sharing"
+    },
+    {
+        id:4,
+        title:"Antusias Ilkomerz Mengikuti Webinar dan Lomba",
+        date:"23/04/22",
+        img:"antusias-webinar-lomba",
+        link: "https://drive.google.com/file/d/1blgMt4JRlLGkq-FajpsJWC9Kr0Y7vxZ4/view?usp=sharing"
+    },
+    {
+        id:5,
+        title:"Evaluasi Pembelajaran Online Ilkomerz",
+        date:"10/06/21",
+        img:"evaluasi-pembelajaran-online",
+        link:"https://drive.google.com/file/d/1k8K5Xx4zfh_3HY794aItL3b8VoJtLHcY/view?usp=sharing"
+    },
+    {
+        id:6,
+        title:"Evaluasi Kinerja Himalkom Setengah Tahun Periode",
+        date:"03/07/21",
+        img:"eval-kinerja-setengah-tahun",
+        link:"https://drive.google.com/drive/folders/1VTr3-z867VN4KmqW2LwPnz9rSNB3Kzpz"
+    },
+    {
+        id:7,
+        title:"Kebiasaan Media Sosial Ilkomerz",
+        date:"22/09/21",
+        img:"kebiasaan-sosial-media",
+        link:"https://drive.google.com/file/d/1pF4NsuxAfcjo3nL0vcSy5xi8TCnBq5su/view?usp=sharing"
+    },
+    {
+        id:8,
+        title:"Penglaman Ilkomerz Menggunakan Website Himalkom",
+        date:"22/09/22",
+        img:"pengalaman-website-himalkom",
+        link:"https://drive.google.com/file/d/1z8IA5i48ZgrO8xbba1eCzJdG5yEXx0Yr/view?usp=sharing"
+
+    },
+    {
+        id:9,
+        title:"Ketertarikan Ilkomerz untuk Membangun Startup",
+        date:"22/02/22",
+        img:"ketertarikan-membangun-startup",
+        link:"https://drive.google.com/file/d/1TqsKRpdotWJUTWWEVSs8VEU9ohBkAfnL/view?usp=sharing"
+
+    }    
+]
---
+++ Frontend/src/component/Syntax/Syntax_card.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { motion } from 'framer-motion';
+import "./Syntax_card.css";
+
+const Syntax_card = (props) => {
+    const redirectToLink = () => {
+        window.open(props.link, '_blank');
+    }
+    return (
+    <motion.div 
+        whileInView={{ y: [30, 10, 0], opacity: [0, 0, 1] }}
+        transition={{duration: 0.5}}
+        className='Syntax-card'
+    >
+        <div className="image-container">
+            <img src={require('../../Assets/Riset/' + props.img + '.png')} alt="Syntax-Icon"/>
+        </div>
+        <h3 className='Judul-Syntax'>{props.title}</h3> 
+        <p className='Syntax-Created'>{props.date}</p> 
+        <p><a href="#" onClick={redirectToLink}>Baca Selengkapnya</a></p>
+    </motion.div>
+    )
+};
+
+export default Syntax_card;
